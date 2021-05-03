e1faf1abbdcf8ab913c68e094fa1f2dbe832c077
Stuart McLean <systemlag@gmail.com>
2021-05-03 17:52:58 +0100

    first commit

---
+++ Soundpaths.lua
@@ -0,0 +1,24 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+-- LSM:Register("sound", "|cFFFF00000X|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\.ogg]])
+LSM:Register("sound", "|cFFFF00000Ducky Ducky|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\ducky-ducky.ogg]])
+LSM:Register("sound", "|cFFFF00000Meatballs|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\meat-a-balls.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
