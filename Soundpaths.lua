5616fa5251eafa4d643921bb30085b91a6eb1131
Stuart <mcleans1995@gmail.com>
2021-12-27 00:50:17 +0000

    hutub request

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -18,6 +18,7 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- LSM:Register("sound", "|cFFFF0000X|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\.ogg]])
 LSM:Register("sound", "|cFFFF0000Ducky Ducky|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\ducky-ducky.ogg]])
 LSM:Register("sound", "|cFFFF0000Meatballs|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\meat-a-balls.ogg]])
+LSM:Register("sound", "|cFFFF0000Travis Scott|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\travis-scott.ogg]])
 
 -- -----
 --   STATUSBAR
