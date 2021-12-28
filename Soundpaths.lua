bbee49856c495969f014e8f65952408d8a2f32da
Stuart <mcleans1995@gmail.com>
2021-12-28 20:42:41 +0000

    more unbearable

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -15,11 +15,11 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- -----
 --   SOUND
 -- -----
--- LSM:Register("sound", "|cFFFF0000X|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\.ogg]])
-LSM:Register("sound", "|cFFFF0000Ducky Ducky|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\ducky-ducky.ogg]])
-LSM:Register("sound", "|cFFFF0000Meatballs|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\meat-a-balls.ogg]])
-LSM:Register("sound", "|cFFFF0000Travis Scott|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\travis-scott.ogg]])
-LSM:Register("sound", "|cFFFF0000Fucks to give|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\fucks-to-give.ogg]])
+-- LSM:Register("sound", "|cFFFFFF00X|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\.ogg]])
+LSM:Register("sound", "|cFFFFFF00Ducky Ducky|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\ducky-ducky.ogg]])
+LSM:Register("sound", "|cFFFFFF00Meatballs|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\meat-a-balls.ogg]])
+LSM:Register("sound", "|cFFFFFF00Travis Scott|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\travis-scott.ogg]])
+LSM:Register("sound", "|cFFFFFF00Fucks to give|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\fucks-to-give.ogg]])
 
 -- -----
 --   STATUSBAR
