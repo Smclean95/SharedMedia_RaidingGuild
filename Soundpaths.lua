0df1bad378ba696977655a65fd43ae35b492980d
Stuart <mcleans1995@gmail.com>
2023-01-29 16:36:43 +0000

    Added the OG thats what mythic is

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -20,6 +20,7 @@ LSM:Register("sound", "|cFFFFFF00Ducky Ducky|r", [[Interface\Addons\SharedMedia_
 LSM:Register("sound", "|cFFFFFF00Meatballs|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\meat-a-balls.ogg]])
 LSM:Register("sound", "|cFFFFFF00Travis Scott|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\travis-scott.ogg]])
 LSM:Register("sound", "|cFFFFFF00Fucks to give|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\fucks-to-give.ogg]])
+LSM:Register("sound", "|cFFFFFF00This is what mythic is|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\this-is-what-mythic-is.ogg]])
 
 -- -----
 --   STATUSBAR
