5d0af5ca757433046c3e93d755b3fe9e5283bd82
Stuart <mcleans1995@gmail.com>
2023-02-12 17:36:00 +0000

    new sounds added

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -21,7 +21,10 @@ LSM:Register("sound", "|cFFFFFF00Meatballs|r", [[Interface\Addons\SharedMedia_Ra
 LSM:Register("sound", "|cFFFFFF00Travis Scott|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\travis-scott.ogg]])
 LSM:Register("sound", "|cFFFFFF00Fucks to give|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\fucks-to-give.ogg]])
 LSM:Register("sound", "|cFFFFFF00This is what mythic is|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\this-is-what-mythic-is.ogg]])
-
+LSM:Register("sound", "|cFFFFFF00Spirit of Christmas|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\eating-ass.ogg]])
+LSM:Register("sound", "|cFFFFFF00FUCK!!..|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\fuck.ogg]])
+LSM:Register("sound", "|cFFFFFF00I'm a piece of garbage|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\im-a-piece-of-garbage.ogg]])
+LSM:Register("sound", "|cFFFFFF00I'm still a piece of garbage|r", [[Interface\Addons\SharedMedia_RaidingGuild\sound\still-garbage.ogg]])
 -- -----
 --   STATUSBAR
 -- -----
